e163b52b68071a8cee4932137b38d64e3ff7cf72
かいじ <71323175+Neteriver@users.noreply.github.com>
2021-12-09 11:17:20 +0900

    Initial Commit

---
+++ seminar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  seminar
+//
+//  Created by 石井快思 on 2021/12/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ seminar/seminarApp.swift
@@ -0,0 +1,17 @@
+//
+//  seminarApp.swift
+//  seminar
+//
+//  Created by 石井快思 on 2021/12/09.
+//
+
+import SwiftUI
+
+@main
+struct seminarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
