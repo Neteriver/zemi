f760bc2044064102a9b3da22ba3012f00954e4e1
かいじ <71323175+Neteriver@users.noreply.github.com>
2021-12-15 12:19:51 +0900

    Create AuthData

---
+++ seminar/AuthData.swift
@@ -0,0 +1,19 @@
+//
+//  AuthData.swift
+//  seminar
+//
+//  Created by 石井快思 on 2021/12/14.
+//
+
+import Foundation
+class LocationAuthData {
+    var message:String = ""
+    
+    var result:Bool = false
+}
+
+class FaceAuthData {
+    var message:String = ""
+    
+    var result:Bool = false
+}
