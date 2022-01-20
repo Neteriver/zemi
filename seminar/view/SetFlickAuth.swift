3746b775fff035268052a1fc419435f6c1c319bf
かいじ <71323175+Neteriver@users.noreply.github.com>
2022-01-20 12:10:48 +0900

    認証設定用ファイルの準備

---
+++ seminar/view/SetFlickAuth.swift
@@ -0,0 +1,22 @@
+//
+//  SetFlickAuth.swift
+//  seminar
+//
+//  Created by 石井快思 on 2022/01/20.
+//
+
+// フリック認証のパスフレーズを設定する画面
+
+import SwiftUI
+
+struct SetFlickAuth: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct SetFlickAuth_Previews: PreviewProvider {
+    static var previews: some View {
+        SetFlickAuth()
+    }
+}
